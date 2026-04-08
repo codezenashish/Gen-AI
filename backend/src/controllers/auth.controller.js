9149d30249f31cb27190795dd9d4791e27a2386d
codezenashish <ixtashish@gmail.com>
2026-04-08 22:54:39 +0530

    fix: Update cookie options for secure and sameSite attributes based on environment

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -83,8 +83,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true,
-    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
   };
 
   return res
@@ -113,7 +113,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
   };
 
   return res
