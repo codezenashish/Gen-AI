4e74d74fceb2dc69a786189ad7536c1424868322
codezenashish <ixtashish@gmail.com>
2026-04-09 22:21:51 +0530

    feat: Update routing to include Protected component for Dashboard and fix import paths

--- frontend/src/auth.routes.jsx
+++ frontend/src/auth.routes.jsx
@@ -2,8 +2,10 @@ import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Dashboard from "./pages/Dashboard";
-import AuthLayout from "./layouts/AuthLayout";
+import AuthLayout from "./layouts/AuthLayout.jsx";
 import ErrorPage from "./pages/ErrorPage";
+import Home from "./features/interview/pages/Home.jsx";
+import Protected from "./features/auth/components/Protected";
 
 export const router = createBrowserRouter([
   {
@@ -11,16 +13,18 @@ export const router = createBrowserRouter([
     element: <AuthLayout />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        index: true,
-        element: <Login />,
-      },
+      { index: true, element: <Login /> },
       { path: "login", element: <Login /> },
       { path: "register", element: <Register /> },
     ],
   },
   {
     path: "/dashboard",
-    element: <Dashboard />,
+    element: (
+      <Protected>
+        <Dashboard />
+      </Protected>
+    ),
   },
+  { path: "/home", element: <Home /> },
 ]);
