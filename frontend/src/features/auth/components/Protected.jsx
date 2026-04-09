34b264e02f454a0cd1715c8282f99ccc486de112
codezenashish <ixtashish@gmail.com>
2026-04-09 22:22:10 +0530

    fix: Correct import path for useAuth in Protected component

--- frontend/src/features/auth/components/Protected.jsx
+++ frontend/src/features/auth/components/Protected.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "../hooks/useAuth.js";
 import { Navigate } from "react-router";
 
 const Protected = ({ children }) => {
