1c226051f582aa4a471f51a162096051340d1f7a
codezenashish <ixtashish@gmail.com>
2026-04-09 22:22:04 +0530

    feat: Add handleRegister function to AuthContext for user registration

--- frontend/src/features/auth/AuthContext.jsx
+++ frontend/src/features/auth/AuthContext.jsx
@@ -1,12 +1,32 @@
 import { createContext, useState } from "react";
+import { registerUser } from "../auth/services/auth.api.js"; 
+
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const handleRegister = async (data) => {
+    setIsLoading(true);
+    try {
+      const response = await registerUser(data);
+      if (response.user) {
+        setUser(response.user);
+        if (response.accessToken) {
+          localStorage.setItem("token", response.accessToken);
+        }
+      }
+      return response;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user, setUser, isLoading, setIsLoading }}>
+    <AuthContext.Provider
+      value={{ user, setUser, isLoading, setIsLoading, handleRegister }}
+    >
       {children}
     </AuthContext.Provider>
   );
