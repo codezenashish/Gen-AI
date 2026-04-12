c603d9f7f7a491bf0432710d18f657a061825e4e
codezenashish <ixtashish@gmail.com>
2026-04-12 12:51:40 +0530

    fix: Correct import statements and format interview router for consistency

--- backend/src/routes/interview.route.js
+++ backend/src/routes/interview.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
-import { upload } from "../middlewares/multer.middleware";
-import { generateInterviewReportController } from "../controllers/interview.controller";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { generateInterviewReportController } from "../controllers/interview.controller.js";
 const interviewRouter = Router();
 
-interviewRouter.route("/").post(verifyJWT,upload.single("resume"), generateInterviewReportController);
+interviewRouter
+  .route("/")
+  .post(verifyJWT, upload.single("resume"), generateInterviewReportController);
 
 export default interviewRouter;
